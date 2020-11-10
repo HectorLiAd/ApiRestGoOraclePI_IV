ffafa095826541d5333a26a751dfde03d5d2b4a6
HectorLiAd <hectorlimahuaya@upeu.edu.pe>
2020-11-10 18:00:53 -0500

    Agregando crods

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/mattn/go-oci8"
 
 	"github.com/ApiRestGoOraclePI_IV/database"
+	"github.com/ApiRestGoOraclePI_IV/helper"
 	"github.com/ApiRestGoOraclePI_IV/persona"
 )
 
@@ -19,51 +20,9 @@ func main() {
 	var personaServicio = persona.NerService(personaRepository)
 
 	r := chi.NewRouter()
+
+	r.Use(helper.GetCors().Handler)
 	r.Mount("/persona", persona.MakeHttpHandler(personaServicio))
 	http.ListenAndServe(":3000", r)
 
-	// pruebasBD(db)
 }
-
-/*
-type addPersonRequest struct {
-	Id               string
-	Nombre           string
-	Apellido_paterno string
-	Apellido_materno string
-	Genero           string
-	Dni              string
-	Fecha_nac        string
-}
-
-func pruebasBD(db *sql.DB) {
-
-	// INSERTAR REGISTROS
-	persona := &addPersonRequest{}
-	persona.Id = "fXXXXXfs"
-	persona.Nombre = "XXXXXXX"
-	persona.Apellido_paterno = "XXXXX"
-	persona.Apellido_materno = "XXXXXX"
-	persona.Genero = "M"
-	persona.Dni = "ffs"
-	persona.Fecha_nac = "27-04-2000"
-	dato := 0
-
-	const sql = `DECLARE
-					ST_PERSONA PERSONA%ROWTYPE;
-				BEGIN
-					ST_PERSONA.NOMBRE := :1;
-					ST_PERSONA.APELLIDO_P := :2;
-					ST_PERSONA.APELLIDO_M := :3 ;
-					ST_PERSONA.GENERO := :4;
-					ST_PERSONA.DNI := :5;
-					ST_PERSONA.FECHA_NACIMIENTO := :6;
-					PKG_CRUD_PERSONA.SPU_AGREGAR_PERSONA(ST_PERSONA, :7);
-				END;`
-	_, err := db.Exec(sql, &persona.Nombre, &persona.Apellido_paterno, &persona.Apellido_materno,
-		&persona.Genero, &persona.Dni, &persona.Fecha_nac, dato)
-	if err != nil {
-		panic(err)
-	}
-
-}*/
